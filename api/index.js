a808d36fdb74b8c92d4a2bdaab08b21ffe51bacb
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-11 12:19:29 -0300

    contrucao da api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import userRoutes from "./routes/users.js";
+import cors from "cors";
 
-const app = express()  /* CONSTRUÇÃO DA API*/
+const app = express();  /* CONSTRUÇÃO DA API*/
 
-app.use(express.json()) /* UTILIZAÇÃO DO MODO JSON PARA POST E PUT */
+app.use(express.json()); /* UTILIZAÇÃO DO MODO JSON PARA POST E PUT */
 
-app.unsubscribe(cors()) /* PARA EVITAR CONFLITOS DE ACESSO RODANDO LOCALMENTE */
+app.use(cors()); /* PARA EVITAR CONFLITOS DE ACESSO RODANDO LOCALMENTE */
 
-app.listen(3000) /* APP ESCUTAR PORTA 3000 */
+app.use("/", userRoutes);
+
+app.listen(3000); /* APP ESCUTAR PORTA 3000 */
