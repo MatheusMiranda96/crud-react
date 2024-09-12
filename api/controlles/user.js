26ae9094ea7c2e115bc2f353523ae50025f9b618
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-12 13:16:01 -0300

    deleteUser

--- api/controlles/user.js
+++ api/controlles/user.js
@@ -28,20 +28,31 @@ export const addUser = (req, res) => {
     }))
 }
 
-export const updateUser = (req, res) => {
-    const q =
-        "UPDATE usuarios SET `nome`= ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+    export const updateUser = (req, res) => {
+        const q =
+            "UPDATE usuarios SET `nome`= ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
 
-    const values = [
-        req.body.nome,
-        req.body.email,
-        req.body.fone,
-        req.body.data_nascimento,
-    ];
+        const values = [
+            req.body.nome,
+            req.body.email,
+            req.body.fone,
+            req.body.data_nascimento,
+        ];
 
-    db.query(q, [...values, req.params.id], (err => {
-        if (err) return res.json(err);
+        db.query(q, [...values, req.params.id], (err => {
+            if (err) return res.json(err);
 
-        return res.status(200).json("Usuário atualizado com sucesso.")
-    }))
-}
+            return res.status(200).json("Usuário atualizado com sucesso.")
+        }))
+    }
+    
+    export const deleteUser = (req, res) => {
+        const q =
+            "DELETE FROM usuarios WHERE `id` = ?";
+
+        db.query(q, [req.params.id], (err => {
+            if (err) return res.json(err);
+
+            return res.status(200).json("Usuário deletado com sucesso.")
+        }))
+    }
