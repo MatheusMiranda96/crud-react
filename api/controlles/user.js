4f9669bcdca2b365074ecc844bbdde97f7766b78
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-12 13:06:04 -0300

    get e add user

--- api/controlles/user.js
+++ api/controlles/user.js
@@ -9,3 +9,21 @@ export const getUsers = (_, res) => { /* Como é um  getUsers não precisamos us
         return res.status(200).json(data);
     })
 }
+
+export const addUser = (req, res) => {
+    const q =
+        "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(q, [values], (err => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso.")
+    }))
+}
