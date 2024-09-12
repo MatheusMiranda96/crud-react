de63fc7605074085fb137e983f915bb6c47ea253
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-12 13:16:32 -0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,27 @@ import './App.css';
 import "react-toastify";
 import Form from './components/Form';
 import Grid from './components/Grid';
+import { useEffect, useState } from 'react';
+import axios from "axios"
 
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try{
+      const res = await axios.get("http://localhost:3000");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  }
+
+    useEffect(() => {
+      getUsers();
+    }, [setUsers]);
   
   return (
      <>
@@ -13,7 +31,7 @@ function App() {
           <h2>USUÁRIOS</h2>
        </div>
        <Form />
-       <Grid />
+       <Grid users={users}/>
        <ToastContainer autoClose={3000} position="bottom-left"/>
      </>
   )
--- frontend/src/components/Grid/index.jsx
+++ frontend/src/components/Grid/index.jsx
@@ -1,16 +1,35 @@
 import styles from './Grid.module.css'
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 
-const Grid = () => {
+
+const Grid = ({ users }) => {
     return(
         <table className={styles.table}>
             <thead>
                 <tr>
                     <th className={styles.th}>Nome</th>
                     <th className={styles.th}>Email</th>
-                    <th className={styles.th}>Fone</th>
+                    <th className={styles.th}>Telefone</th>
+                    <th className={styles.th}></th>
+                    <th className={styles.th}></th>
                 </tr>
             </thead>
+            <tbody>
+              {users.map((item, i) => (
+                <tr key={i}>
+                    <td className={styles.td}>{item.nome}</td>
+                    <td className={styles.td}>{item.email}</td>
+                    <td className={styles.td}>{item.fone}</td>
+                    <td className={styles.td}>
+                        <FaEdit />
+                    </td>
+                    <td className={styles.td}>
+                        <FaTrash />
+                    </td>
+                </tr>
+              ))}
+            </tbody>
         </table>
     )
 }
