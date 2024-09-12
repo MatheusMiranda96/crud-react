02b49ef822116e469b9091bcffe9d3c1466a9499
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-12 13:12:07 -0300

    updateUser

--- api/controlles/user.js
+++ api/controlles/user.js
@@ -27,3 +27,21 @@ export const addUser = (req, res) => {
         return res.status(200).json("Usuário criado com sucesso.")
     }))
 }
+
+export const updateUser = (req, res) => {
+    const q =
+        "UPDATE usuarios SET `nome`= ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(q, [...values, req.params.id], (err => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário atualizado com sucesso.")
+    }))
+}
